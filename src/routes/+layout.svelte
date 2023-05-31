9379551770c78cc1831a13dc7db765c3c03e0048
ajeetk23 <96801265+ajeetk23@users.noreply.github.com>
2023-05-30 17:58:54 -0700

    Add files via upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import '../app.postcss';
+</script>
+
+<div class="bg-gray-100 min-h-screen overflow-y-auto">
+  <div class="flex flex-col h-screen">
+    <slot />
+    <footer class="footer footer-center p-4 bg-base-300 text-base-content">
+      <div>
+        <p>Copyright © 2023 Bankify</p>
+      </div>
+    </footer>
+  </div>
+</div>
